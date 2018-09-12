b64458019c02dad1413d62e6aaead2e1e226566e
RameshBettge <bettge.r@gmx.de>
2018-09-12 13:59:20 +0200

    Improved: Snapping now properly works on the x Axis.

--- Assets/_Scripts/CircleGenerator.cs
+++ Assets/_Scripts/CircleGenerator.cs
@@ -38,11 +38,6 @@ public class CircleGenerator : MonoBehaviour
 
     Vector3 middlePos;
 
-    public int[] corners = new int[4]; //Top Right, Botton Right, Bottom Left, Top Left
-    public bool[] cornerHit = new bool[4];
-
-    public enum Corners { TopRight = 0, BottomRight = 1, BottomLeft = 2, TopLeft = 3 }
-
     void Start()
     {
         //Prepare Circle
@@ -56,13 +51,6 @@ public class CircleGenerator : MonoBehaviour
         hits = new RaycastHit[rayDensity];
         dir = new Vector3[rayDensity];
         hasHit = new bool[rayDensity];
-
-
-        //Get Corners
-        for (int i = 0; i < 4; i++)
-        {
-            corners[i] = (rayDensity / 8) + (rayDensity / 4) * i;
-        }
 
         //Get ray directions
         increment = 360f / rayDensity;
@@ -94,29 +82,24 @@ public class CircleGenerator : MonoBehaviour
         for (int i = 0; i < rayDensity; i++)
         {
             Vector3 localDir = transform.TransformDirection(dir[i]);
-            if (Physics.Raycast(transform.position, localDir * range, out hits[i], mask))
+            if (Physics.Raycast(transform.position, localDir * range, out hits[i], mask) 
+                && Vector3.Dot(transform.forward, -hits[i].normal) > 0.4f) 
+                //second condition avoids creating a mesh on walls which are nearly perpendicular to light direction
             {
                 if (displayRays)
                 {
                     Debug.DrawRay(transform.position, localDir * hits[i].distance);
                 }
+
                 hasHit[i] = true;
                 currentHits++;
+       
             }
             else
             {
                 hasHit[i] = false;
             }
-
-            for (int c = 0; c < 4; c++)
-            {
-                if (i == corners[c])
-                {
-                    cornerHit[c] = hasHit[i];
-                }
-            }
         }
-
     }
 
     private void CreateMesh()
@@ -136,8 +119,6 @@ public class CircleGenerator : MonoBehaviour
 
         //Get middle Position
         middlePos = GetMiddlePosition();
-
-
         vertices[0] = middlePos;
 
         //normalize the positions of the vertices. Middle = Vector3.zero. Then Move the mesh to the object hit
@@ -147,8 +128,6 @@ public class CircleGenerator : MonoBehaviour
         }
         circle.position = middlePos;
 
-        //Set the middle Vertex to the edge of surface's bounding box.
-        //vertices[0] = GetBoundingPosition(midHit, middlePos);
 
         //Create triangles
         int triIndex = 0;
@@ -169,26 +148,6 @@ public class CircleGenerator : MonoBehaviour
         mesh.triangles = tris;
 
         filter.mesh = mesh;
-    }
-
-    private Vector3 GetTopLeft()
-    {
-        float furthestUp = 0f;
-        float furthestLeft = 0f;
-
-        for (int i = 1; i < vertices.Length; i++)
-        {
-            if (vertices[i].x < furthestLeft)
-            {
-                furthestLeft = vertices[i].x;
-            }
-            if (vertices[i].y > furthestUp)
-            {
-                furthestUp = vertices[i].y;
-            }
-        }
-        Vector3 target = new Vector3(furthestLeft, furthestUp, 0f);
-        return target;
     }
 
     Vector3 GetMiddlePosition()
@@ -233,9 +192,8 @@ public class CircleGenerator : MonoBehaviour
         Vector3 halfExtends = hit.collider.bounds.extents;
         float avgRadius = 0f;
 
-        //Vector3 localPoint = hit.collider.transform.InverseTransformPoint(hit.point);
-        Vector3 localPoint = hit.collider.transform.InverseTransformDirection(hit.point);
-        //Vector3 localPoint = hit.point - hit.collider.transform.position;
+        Vector3 localPoint = hit.collider.transform.InverseTransformPoint(hit.point);
+        Vector3 scaledPoint = Vector3.Scale(localPoint, halfExtends);
 
         for (int i = 1; i < vertices.Length; i++)
         {
@@ -246,25 +204,28 @@ public class CircleGenerator : MonoBehaviour
 
         //// WARNING: ONLY FOR DEBUGGING:
         //avgRadius = 100f;
+        //avgRadius *= 6;
         //// END WARNING
 
-        
+        //float rightDist = halfExtends.x - localPoint.x;
+        //float leftDist = Mathf.Abs(-halfExtends.x - localPoint.x);
 
-        float rightDist = halfExtends.x - localPoint.x;
-        float leftDist = Mathf.Abs(-halfExtends.x - localPoint.x);
+        //float rightDist2 = halfExtends.x - scaledPoint.x;
+        //float leftDist2 = Mathf.Abs(-halfExtends.x - scaledPoint.x);
 
-        if (rightDist < avgRadius && rightDist < leftDist)
+        float rightDist = 0.5f - localPoint.x;
+        float leftDist = Mathf.Abs(-0.5f - localPoint.x);
+        float xRadius = avgRadius / halfExtends.x / 2f;
+
+        if (rightDist < xRadius && rightDist < leftDist)
         {
-            localPoint.x = halfExtends.x;
-            //o = hit.collider.transform.TransformPoint(localPoint);
-            o = hit.collider.transform.TransformDirection(localPoint);
-            //o = hit.point + hit.collider.transform.position;
+            localPoint.x = 0.5f;
+            o = hit.collider.transform.TransformPoint(localPoint);
         }
-        else if (leftDist < avgRadius)
+        else if (leftDist < xRadius)
         {
-            localPoint.x = -halfExtends.x;
-            o = hit.collider.transform.TransformDirection(localPoint);
-            //o = hit.point + hit.collider.transform.position;
+            localPoint.x = -0.5f;
+            o = hit.collider.transform.TransformPoint(localPoint);
         }
 
         float frontDist = halfExtends.z - localPoint.z;
